5862d130894beb07223bfde3ea7aa24a41b8ceb3
sandzotym <93154062+sandzotym@users.noreply.github.com>
2021-12-22 20:36:44 +0530

    Delete order-service directory

--- order-service/src/main/java/com/microservice/os/api/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.microservice.os.api.repository;
-
-import com.microservice.os.api.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-}
