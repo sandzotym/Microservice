61ec7b3c9d1e320bc1e1f886b515892e39b28078
sandzotym <93154062+sandzotym@users.noreply.github.com>
2021-12-22 20:36:55 +0530

    Delete payment-service directory

--- payment-service/src/main/java/com/microservice/ps/api/repository/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.microservice.ps.api.repository;
-
-import com.microservice.ps.api.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Integer> {
-    Payment findByOrderId(String orderId);
-}
